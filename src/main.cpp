c160a645def25cb0adb40814b36956a380324200
Paweł Marciniak <sunwire+git@gmail.com>
2022-02-05 12:09:34 +0100

    qmake prefix support and translations fix

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,22 @@
 #include <QApplication>
 #include <QTranslator>
 
+#ifndef INSTALL_PREFIX
+#define INSTALL_PREFIX "/usr/local"
+#endif
+
+#define xstr(s) str(s)
+#define str(s) #s
+
 // -----------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
 	QTranslator trans;
-	trans.load(QLocale(), "krokonf", "_", ".");
+	if (!trans.load(QLocale(), "krokonf", "_", xstr (INSTALL_PREFIX)"/share/krokonf/translations/")) {
+		trans.load(QLocale(), "krokonf", "_", ".");
+	}
 	a.installTranslator(&trans);
 	CrookConf c;
 	MainWindow w(&c);
